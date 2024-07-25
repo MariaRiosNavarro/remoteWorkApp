603e27cfd2f8069e7c43280f09610379f9598e6e
Maria Rios Navarro <propscode254@gmail.com>
2024-07-25 19:09:59 +0200

    create a put route without send email

--- backend/src/controller/time.controller.js
+++ backend/src/controller/time.controller.js
@@ -1,6 +1,6 @@
 import { TimeModel } from "./../models/time.model.js";
 import { UserModel } from "./../models/user.model.js";
-
+import { calculateDuration } from "../utils/calculateDuration.js";
 
 /**
  * Öffnet eine neue Homeoffice-Zeiterfassung für einen Benutzer.
@@ -40,4 +40,52 @@ export const openTime = async (req, res) => {
   }
 };
 
+/**
+ * Schließt eine offene Homeoffice-Zeiterfassung.
+ */
+export const closeTime = async (req, res) => {
+  try {
+    const { userId, timeId } = req.params;
 
+    // Offenen Zeiteintrag suchen
+    const time = await TimeModel.findOne({
+      _id: timeId,
+      userIdRef: userId,
+      status: "open",
+    });
+    if (!time) {
+      return res
+        .status(404)
+        .json({ message: "Kein offener Zeiteintrag gefunden" });
+    }
+
+    // Zeiteintrag schließen
+    time.status = "close";
+    time.endTime = new Date();
+
+    let durationTime = calculateDuration(time.startTime, time.endTime);
+
+    // const durationTime = (time.endTime - time.startTime) / (1000 * 60 * 60);
+
+    time.durationTime = durationTime;
+
+    await time.save();
+
+    // Benutzerinformationen abrufen
+    const user = await UserModel.findById(userId);
+
+    res.status(200).json({
+      success: true,
+      data: time,
+      message: "Homeoffice-Zeit erfolgreich beendet ✅",
+    });
+  } catch (error) {
+    console.error("Fehler beim Schließen der Homeoffice-Zeit:", error);
+    res.status(500).json({
+      success: false,
+      message:
+        "Fehler beim Schließen der Homeoffice-Zeit oder Senden der Benachrichtigung",
+      error: error.message,
+    });
+  }
+};
--- backend/src/routes/time.routes.js
+++ backend/src/routes/time.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { openTime } from "./../controller/time.controller.js";
+import { openTime, closeTime } from "./../controller/time.controller.js";
 import { verifyUser } from "../middlewares/auth.middlewares.js";
 
 export const router = new express.Router();
 
 router.post("/:userId", verifyUser, openTime);
+router.put("/:userId/:timeId", verifyUser, closeTime);
