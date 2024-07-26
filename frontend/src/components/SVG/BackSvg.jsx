8b02a77fe5feb8c5b69f09b80211d561adb133ee
Maria Rios Navarro <propscode254@gmail.com>
2024-07-26 08:00:00 +0200

    add a SVG component Bundeln (from another Proyect) to ease and quickle use

---
+++ frontend/src/components/SVG/BackSvg.jsx
@@ -0,0 +1,15 @@
+const BackSvg = () => {
+  return (
+    <svg
+      className="fill-base-100 stroke-base-100"
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+    >
+      <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+    </svg>
+  );
+};
+
+export default BackSvg;
